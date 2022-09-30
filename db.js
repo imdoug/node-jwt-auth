2ae97c0cfc49989306f63147336123d1a7683fb8
Doug Moreira <dougmoreira@Dougs-MBP-1066.lan>
2022-09-30 18:58:36 -0400

    prod changes

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const connectionString = `postgresql://dougmoreira:amor1512@localhost:5432/users
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
+  ssl: { rejectUnauthorized: false },
 })
 
 module.exports =  pool 
